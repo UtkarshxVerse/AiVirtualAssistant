0c105602319d7303afd3307e194612f0afa7dcb1
Utkarsh <utkarsh2423@gmail.com>
2025-11-05 13:07:57 +0530

    Update CORS origin to production URL

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ import geminiResponse from './gemini.js';
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://aivirtualassistant-1h61.onrender.com",
     credentials: true
 }
 ));
